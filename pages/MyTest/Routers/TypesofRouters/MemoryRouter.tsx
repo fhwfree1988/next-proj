f597f70e707c3ab19cc8a54589a5b3937f3e41f3
Foad <FuadHr>
2022-01-04 18:20:36 +0330

    change

--- pages/MyTest/Routers/TypesofRouters/MemoryRouter.tsx
+++ pages/MyTest/Routers/TypesofRouters/MemoryRouter.tsx
@@ -2,3 +2,4 @@
 //https://www.geeksforgeeks.org/react-router-hooks/
 //https://blog.logrocket.com/using-hooks-with-react-router/
 //https://v5.reactrouter.com/web/api/Hooks
+//https://blog.logrocket.com/improve-modal-management-react-nice-modal-react/
